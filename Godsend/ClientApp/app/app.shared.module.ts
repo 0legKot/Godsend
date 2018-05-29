efab8ecaf7281266cd6022f49e0dc82c29ae5ef5
Alek <alek40@i.ua>
2018-05-29 22:12:02 +0300

    updated to HttpClient from Http

--- Godsend/ClientApp/app/app.shared.module.ts
+++ Godsend/ClientApp/app/app.shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
@@ -34,7 +34,7 @@ import { SuppliersComponent } from './components/suppliers/suppliers.component';
     ],
     imports: [
         CommonModule,
-        HttpModule,
+        HttpClientModule,
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
--- Godsend/ClientApp/app/models/repository.ts
+++ Godsend/ClientApp/app/models/repository.ts
@@ -1,6 +1,6 @@
 ﻿import { Product } from "./product.model";
 import { Injectable, Inject } from "@angular/core";
-import { Http, RequestMethod, Request, Response } from "@angular/http";
+import { HttpClient } from "@angular/common/http"
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
@@ -14,22 +14,23 @@ const ordersUrl = "/api/order";
 @Injectable()
 export class Repository {
 
-    constructor(private http: Http, @Inject('BASE_URL') private baseUrl: string) {
+    constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
         this.getProducts();
     }
 
     getProduct(id: string) {
-        this.sendRequest(RequestMethod.Get, productsUrl + "/" + id)
+        this.sendRequest<Product>('get', productsUrl + "/" + id)
             .subscribe(response => this.product = response);
     }
 
     getProducts() {
         let url = productsUrl;
-        
-        this.sendRequest(RequestMethod.Get, url + "/all")
+
+        this.sendRequest<Product[]>('get', url + '/all')
             .subscribe(response => {
-                this.products = response;
-            });
+            this.products = response;
+        });
+        
     }
 
     createProduct(prod: Product) {
@@ -38,7 +39,7 @@ export class Repository {
             description: prod.info.description
         };
 
-        this.sendRequest(RequestMethod.Post, productsUrl, data)
+        this.sendRequest<string>('post', productsUrl, data)
             .subscribe(response => {
                 prod.id = response;
                 this.products.push(prod);
@@ -53,50 +54,45 @@ export class Repository {
             description: prod.info.description
 
         };
-        this.sendRequest(RequestMethod.Put, productsUrl + "/" + prod.id, data)
+        this.sendRequest<null>('put', productsUrl + "/" + prod.id, data)
             .subscribe(response => this.getProducts());
     }
-
+    
     
 
-    private sendRequest(verb: RequestMethod, url: string, data?: any)
-        : Observable<any> {
+    private sendRequest<T>(method: string, url: string, data?: any)
+        : Observable<T> {
 
-        return this.http.request(new Request({
-            method: verb, url: this.baseUrl+url, body: data
-        })).map(response => {
-            if (response.headers != null) {
+        return this.http.request<T>(method, url, { body: data, responseType: "json" })
+            .map(response => {
                 console.log(response);
-                return response.headers.get("Content-Length") != "0"
-                    ? response.json() : null;
-            }
-            else return null;
-        });
+                return response;
+            });
     }
-
+    
     updateProduct(id: string, changes: Map<string, any>) {
         var patch:any[]=[];
         changes.forEach((value, key) =>
             patch.push({ op: "replace", path: key, value: value }));
 
-        this.sendRequest(RequestMethod.Patch, productsUrl + "/" + id, patch)
+        this.sendRequest<null>('patch', productsUrl + "/" + id, patch)
             .subscribe(response => this.getProducts());
     }
 
     deleteProduct(id: string) {
-        this.sendRequest(RequestMethod.Delete, productsUrl + "/" + id)
+        this.sendRequest<null>('delete', productsUrl + "/" + id)
             .subscribe(response => this.getProducts());
     }
 
 
 
     storeSessionData(dataType: string, data: any) {
-        return this.sendRequest(RequestMethod.Post, "/api/session/" + dataType, data)
+        return this.sendRequest<null>('post', "/api/session/" + dataType, data)
             .subscribe(response => { });
     }
 
     getSessionData(dataType: string): Observable<any> {
-        return this.sendRequest(RequestMethod.Get, "/api/session/" + dataType);
+        return this.sendRequest<any>('get', "/api/session/" + dataType);
     }
 
 
