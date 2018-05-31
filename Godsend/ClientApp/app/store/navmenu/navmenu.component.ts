2afccb63c1288d99ea655690b3cd3c2306347672
KseniyaOsadcha <theksenic@gmail.com>
2018-05-31 14:59:40 +0300

    added styles for the menu

--- Godsend/ClientApp/app/store/navmenu/navmenu.component.ts
+++ Godsend/ClientApp/app/store/navmenu/navmenu.component.ts
@@ -7,12 +7,27 @@ import { Component } from '@angular/core';
 })
 export class NavMenuComponent {
 
-    userData: DummyUserData = { name: "Admin", isAdmin: true};
+    userData: DummyUserData = { name: "Admin", isAdmin: true };
+    
 
     scrollToTop(): void {
         window.scrollTo(0, 0);
     }
-
+    bool = true;
+    slideToggle(): void {
+        let tmp = document.getElementById('_nav');
+        if (tmp) {
+            if (this.bool) {
+                tmp.style.display = "block";
+                this.bool = false;
+            }
+            else if (this.bool == false){
+                tmp.style.display = "none";
+                this.bool = true;
+            }
+            console.log(this.bool);
+        }
+    }
     logout() {
         this.userData = {};
     }
