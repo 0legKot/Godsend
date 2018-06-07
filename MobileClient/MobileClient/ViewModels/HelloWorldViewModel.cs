101969a300ca49d4848ea95ec5271c2427e840c3
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-06-08 01:31:56 +0300

    Created HelloWorldViewModel in MobileClient.ViewModels. Edited HelloWorldPage in Views. Add migrations.

---
+++ MobileClient/MobileClient/ViewModels/HelloWorldViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+namespace MobileClient.ViewModels
+{
+    public class HelloWorldViewModel : BaseViewModel
+    {
+        public HelloWorldViewModel()
+        {
+            Title = "Hello there";            
+        }        
+    }
+}
