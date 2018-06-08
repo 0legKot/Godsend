132e7fd5f8f1b79ec4116bb01f1b5c0c053dcb1d
Oleg <kronosgod12@gmail.com>
2018-06-08 09:49:44 +0300

    merge

--- MobileClient/MobileClient.iOS/Main.cs
+++ MobileClient/MobileClient.iOS/Main.cs
@@ -13,7 +13,7 @@ namespace MobileClient.iOS
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
+            // you can specify it here.  
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
