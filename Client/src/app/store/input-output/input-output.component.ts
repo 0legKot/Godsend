aa6fae06057e720e6b610c52727a3bcec910aea6
Alek <alek40@i.ua>
2018-06-13 12:52:32 +0300

    input-output component

---
+++ Client/src/app/store/input-output/input-output.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+    selector: 'godsend-input-output',
+    templateUrl: './input-output.component.html',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: InputOutputComponent,
+            multi: true
+        }
+    ]
+})
+export class InputOutputComponent implements OnInit, ControlValueAccessor {
+    @Input()
+    edit = false;
+
+    value = '';
+
+    onChange = (_: string) => { };
+
+    onTouched = (_: string) => { };
+
+    constructor() { }
+
+    changeValue(newValue: string) {
+        this.value = newValue;
+        this.onChange(this.value);
+    }
+
+    writeValue(obj: string): void {
+        this.value = obj;
+    }
+
+    registerOnChange(fn: (_:string) => any): void {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any): void {
+        this.onTouched = fn;
+    }
+
+    ngOnInit() {
+    }
+}
