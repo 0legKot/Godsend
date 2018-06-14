530f1e2c853b8046f9da233f726ecf2ef651e14f
Alek <alek40@i.ua>
2018-06-14 21:37:55 +0300

    Revert "gitignore test"
    This reverts commit 99b628432a4bba67be38de069f0620cec2cca50a.

--- Client/src/app/models/cart.model.js
+++ Client/src/app/models/cart.model.js
@@ -8,7 +8,6 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-
 export var guidZero = '00000000-0000-0000-0000-000000000000';
 var Cart = /** @class */ (function () {
     function Cart(discreteItems) {
