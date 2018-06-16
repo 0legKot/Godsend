b6034af0f2266e284531d69bf27c994d7ac42778
Alek <alek40@i.ua>
2018-06-16 14:55:47 +0300

    added basis for image processing

---
+++ Client/src/app/services/image.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from "@angular/core";
+import { DataService } from "./data.service";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/internal/operators/map";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ImageService {
+    constructor(private data: DataService) { }
+
+    getImages(id: string, fn: (_: string[]) => any): void {
+        // use ID
+        this.data.sendRequest<string[]>('get','api/image/getImages')
+            .subscribe(response => fn(response));
+    }
+}
