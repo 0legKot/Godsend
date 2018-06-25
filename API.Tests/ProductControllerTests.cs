a62a420c4565ed264e7fcf2e2a0b63bc7af45e18
Alek <alek40@i.ua>
2018-06-25 18:18:48 +0300

    added test project

---
+++ API.Tests/ProductControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Godsend;
+using Godsend.Controllers;
+using Godsend.Models;
+using Moq;
+using System;
+using Xunit;
+
+namespace API.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void DetailTest()
+        {
+            var repo = new Mock<IProductRepository>();
+
+            repo.Setup(x => x.GetProductWithSuppliers(It.IsAny<Guid>()))
+                .Returns(new ProductWithSuppliers { Product = new Product {Name = "Test" } });
+
+            var controller = new ProductController(repo.Object);
+
+            var result = controller.Detail(It.IsAny<Guid>());
+
+            Assert.True(result.Product.Name == "Test");
+        }
+    }
+}
