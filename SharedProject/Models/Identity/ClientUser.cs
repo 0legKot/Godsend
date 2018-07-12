ce729db18424739231010529ae6faff627dafdc3
s.redenskiy <alek40@i.ua>
2018-07-12 16:50:34 +0300

    Changed client model on server

--- SharedProject/Models/Identity/ClientUser.cs
+++ SharedProject/Models/Identity/ClientUser.cs
@@ -29,5 +29,37 @@ namespace Godsend.Models
         /// The name.
         /// </value>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets the birth.
+        /// </summary>
+        /// <value>
+        /// The birth.
+        /// </value>
+        public DateTime Birth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the registration date.
+        /// </summary>
+        /// <value>
+        /// The registration date.
+        /// </value>
+        public DateTime RegistrationDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the rating.
+        /// </summary>
+        /// <value>
+        /// The rating.
+        /// </value>
+        public int Rating { get; set; }
     }
 }
