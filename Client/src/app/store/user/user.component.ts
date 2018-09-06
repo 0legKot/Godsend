093a623ed161173a2bc6e8f90f214d5848ed2dd8
Oleg <kronosgod12@gmail.com>
2018-09-06 17:23:01 +0300

    Add user crud on client

--- Client/src/app/store/user/user.component.ts
+++ Client/src/app/store/user/user.component.ts
@@ -12,12 +12,55 @@ import { StorageService } from '../../services/storage.service';
 export class UserComponent implements OnInit {
     user: IdentityUser = new IdentityUser();
     name = this.storage.name;
+    edit: boolean = false;
+    backup: UserBackup = {
+        name: '',
+        email: '',
+        birth: ''
+    };
 
     constructor(private storage: StorageService, private data: DataService) { }
+
+    save() {
+        if (this.user) {
+            this.data.sendRequest<IdentityUser>('post', 'api/account/editProfile/' + this.user.id, this.user).subscribe(u => this.user = u);
+            //this.repo.createOrEditEntity('user', User.EnsureType(this.user), 1, 10);
+        }
+
+        this.edit = false;
+    }
+
+    discard() {
+        if (this.user) {
+            this.user.name = this.backup.name;
+            this.user.email = this.backup.email;
+            this.user.birth = this.backup.birth;
+        }
+
+        this.edit = false;
+    }
+
+    editMode() {
+        if (this.user == null) {
+            console.log('no data');
+        } else {
+            this.backup = {
+                name: this.user.name,
+                email: this.user.email,
+                birth: this.user.birth
+            };
+            this.edit = true;
+        }
+    }
 
     ngOnInit(): void {
         this.data.sendRequest<IdentityUser>('get', 'api/account/getprofile/' + this.name).subscribe(
             res => this.user = res
         );
     }
+}
+interface UserBackup {
+    name: string;
+    email: string;
+    birth: string;
 }
