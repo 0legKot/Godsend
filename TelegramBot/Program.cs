55a9c1fccecbebecad265be4937db17650ee8953
s.redenskiy <alek40@i.ua>
2018-09-12 21:08:29 +0300

    added categories output for bot

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using Telegram;
 using Newtonsoft.Json;
@@ -36,14 +37,12 @@ namespace TelegramBot
                                 Bot.SendTextMessageAsync(message.Chat.Id, "Hello world! I am great, yeah", replyToMessageId: message.MessageId).GetAwaiter();
                                 break;
                             case "/products":
-                                var msg = "";
+                                var msg = "Our categories:\n";
                                 var client = new HttpClient();
                                 Stream respStream = client.GetStreamAsync(uri).GetAwaiter().GetResult();
-                                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(IEnumerable<CatWithSubs>));
-                                IEnumerable<CatWithSubs> feed = (IEnumerable<CatWithSubs>)ser.ReadObject(respStream);
-                                int i = 0;
-                                foreach (var cat in feed) i++;
-                                msg = $"We have {i} categories!";
+                                string json = new StreamReader(respStream).ReadToEnd();
+                                var cats = JsonConvert.DeserializeObject<IEnumerable<CatWithSubs>>(json);
+                                msg += GetFormattedCategoryTree(cats);
                                 Bot.SendTextMessageAsync(message.Chat.Id, msg, replyToMessageId: message.MessageId).GetAwaiter();
                                 break;
                             case "/suppliers":
@@ -56,11 +55,44 @@ namespace TelegramBot
                                 Bot.SendTextMessageAsync(message.Chat.Id, "You wrote: " + message.Text, replyToMessageId: message.MessageId).GetAwaiter();
                                 break;
                         }
-                        
                     }
                     offset = update.Id + 1;
                 }
 
+            }
+        }
+
+        static string GetFormattedCategoryTree(IEnumerable<CatWithSubs> rootCats)
+        {
+            var res = "";
+            var s = new LinkedList<string>();
+            foreach (var cat in rootCats)
+            {
+                res += $"{cat.Cat.Name}\n";
+                res += GetFormattedCategoryTreeRecursive(cat.Subs, 0);
+            }
+
+            return res;
+
+            string GetFormattedCategoryTreeRecursive(IEnumerable<CatWithSubs> curCats, int level)
+            {
+                var str = "";
+                var arrCats = curCats.ToArray();
+                for (int i = 0; i < arrCats.Length; ++i)
+                {
+                    if (s.Any())
+                    {
+                        str += s.Aggregate((a, b) => a + b);
+                    }
+                    str += i == arrCats.Length - 1 ? "└" : "├";
+                    str += $"{arrCats[i].Cat.Name}\n";
+
+                    s.AddLast(i == arrCats.Length - 1 ? "─" : "│");
+                    str += GetFormattedCategoryTreeRecursive(arrCats[i].Subs, level + 1);
+                    s.RemoveLast();
+                }
+
+                return str;
             }
         }
     }
